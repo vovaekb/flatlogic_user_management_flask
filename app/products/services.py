a947964d849b0cea56dbfd30a331947459bc2416
unknown <vovaprivalov@gmail.com>
2021-04-09 23:12:45 +0200

    Add skeletone for Products service

--- app/products/services.py
+++ app/products/services.py
@@ -1,1 +1,28 @@
-# TBA
+import os
+from sqlalchemy.sql import func
+from sqlalchemy.exc import SQLAlchemyError
+from app import app, mail
+from app.models import Users, Files
+from app import CustomError
+from app.users.db import UserDBApi
+from app.auth.services import Auth
+from app.serializers import ProductsSchema
+
+class ProductService:
+    def get_images():
+        pass
+
+    def get_products():
+        pass
+
+    def get_product():
+        pass
+
+    def update_roduct():
+        pass
+
+    def create_product():
+        pass
+
+    def delete_product():
+        pass
