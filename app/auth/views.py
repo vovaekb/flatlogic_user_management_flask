f031226a18c520f052e170978659ac02bc498de4
Vladimir Privalov <vladprivalov1990@gmail.com>
2021-04-15 16:39:53 +0200

    Remove redundant setting CORS headers in auth/signin endpoint

--- app/auth/views.py
+++ app/auth/views.py
@@ -22,18 +22,6 @@ def handle_error(e):
     return Response(details['message'], status=200, mimetype='text/plain')
 
 
-def build_preflight_response():
-    print('build_preflight_response')
-    response = Response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "OPTIONS, POST") # "*")
-    return response
-
-def build_actual_response(response):
-    print('build_actual_response')
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
 
 # ROUTES
 @auth_blueprint.route('/auth/password-reset', methods=['PUT'])
@@ -80,16 +68,9 @@ def send_password_reset_email():
 @auth_blueprint.route('/api/auth/signin/local', methods=['POST']) #, 'OPTIONS'])
 @cross_origin(supports_credentials=True)
 def signin_local():
-    '''
-    if request.method == 'OPTIONS':
-        print('OPTIONS ACCEPTED')
-        return build_preflight_response()
-    if request.method == 'POST':
-    '''
     print('POST ACCEPTED')
     payload = Auth.signin(request.json['email'], request.json['password'])
     resp = Response(payload, status=200)
-    build_actual_response(resp)
     return resp # Response(payload, status=200)
 
 @auth_blueprint.route('/auth/signup', methods=['POST'])
