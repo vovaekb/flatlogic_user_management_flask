6ddce8178099fb846fbfc15e07ea04fc8798069a
unknown <vovaprivalov@gmail.com>
2021-04-21 19:15:29 +0200

    Add product seeders

--- setup_db.py
+++ setup_db.py
@@ -57,6 +57,227 @@ def seed_products():
             updatedAt=func.now()
         )
         s.add(product)
+        # product 2
+        product = Products(
+            title="boots",
+            subtitle='Trainers In Blue',
+            img='%s/assets/products/img2.jpg' % app.config['REMOTE'],
+            price=45,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=20,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 3
+        product = Products(
+            title="flat sandals",
+            subtitle='Trainers In White',
+            img='%s/assets/products/img3.jpg' % app.config['REMOTE'],
+            price=55,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=None,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 4
+        product = Products(
+            title="trainers",
+            subtitle='Trainers In White',
+            img='%s/assets/products/img4.jpg' % app.config['REMOTE'],
+            price=76,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=None,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 5
+        product = Products(
+            title="boots",
+            subtitle='Trainers In Blue',
+            img='%s/assets/products/img5.jpeg' % app.config['REMOTE'],
+            price=45,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=20,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 6
+        product = Products(
+            title="flat sandals",
+            subtitle='Trainers In White',
+            img='%s/assets/products/img6.jpg' % app.config['REMOTE'],
+            price=55,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=None,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 7
+        product = Products(
+            title="trainers",
+            subtitle='Trainers In White',
+            img='%s/assets/products/img1.jpg' % app.config['REMOTE'],
+            price=76,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                    "Ollie patch",
+                    "Cup soles",
+                    "Vulcanized rubber soles"
+                ],
+            discount=None,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 8
+        product = Products(
+            title="boots",
+            subtitle='Trainers In Blue',
+            img='%s/assets/products/img2.jpg' % app.config['REMOTE'],
+            price=45,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=20,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 9
+        product = Products(
+            title="flat sandals",
+            subtitle='Trainers In White',
+            img='%s/assets/products/img3.jpg' % app.config['REMOTE'],
+            price=55,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=None,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 10
+        product = Products(
+            title="trainers",
+            subtitle='Trainers In White',
+            img='%s/assets/products/img4.jpg' % app.config['REMOTE'],
+            price=76,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=None,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 11
+        product = Products(
+            title="boots",
+            subtitle='Trainers In Blue',
+            img='%s/assets/products/img5.jpeg' % app.config['REMOTE'],
+            price=45,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=20,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+        # product 12
+        product = Products(
+            title="flat sandals",
+            subtitle='Trainers In White',
+            img='%s/assets/products/img6.jpg' % app.config['REMOTE'],
+            price=55,
+            rating=4.6,
+            code=135234,
+            hashtag="whitetrainers",
+            technology=[
+                "Ollie patch",
+                "Cup soles",
+                "Vulcanized rubber soles"
+            ],
+            discount=None,
+            description_1="Sneakers (also known as athletic shoes, tennis shoes,gym shoes, runners, takkies, or trainers) are shoes primarily designed for sports or other forms of physical exercise, but which are now also often used for everyday wear.",
+            description_2="The term generally describes a type of footwear with a flexible sole made of rubber or synthetic material and an upper part made of leather or synthetic materials.",
+            updatedAt=func.now()
+        )
+        s.add(product)
+
 
 def seed_users():
     print('Seed users')
