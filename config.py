db5665835747c313cea7e1a73722f55155bace6f
Vladimir Privalov <vovaprivalov@gmail.com>
2023-05-17 11:47:07 +0400

    .

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-DATABASE_URI = "postgres+psycopg2://postgres:123@localhost/user_management"
+DATABASE_URI = os.environ.get("DB_PROD")
 
 providers = {
     "LOCAL": "local",
@@ -12,20 +11,19 @@ providers = {
 class Config:
     UPLOAD_FOLDER = 'static/'
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024
-    SECRET_KEY = "HUEyqESqgQ1yTwzVlO6wprC9Kf1J1xuA" # "\x88\xc7\x12I\xc1\x8b\xcf\xc5\x16\xc2\xefG\x92}\x8e\xe84Y\x19\x8d\xc7\xdd9\xbd"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     APP_TITLE = 'Application'
-    MAIL_SERVER = "smtp.googlemail.com" # os.environ.get('MAIL_SERVER')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = 587
-    MAIL_USE_TLS=1
+    MAIL_USE_TLS = 1
     MAIL_DEFAULT_SENDER = "support@flatlogic.com"
     MAIL_USERNAME = "support@flatlogic.com"
-    MAIL_PASSWORD="UBU2JGC2wEqc"
-    # Google Auth parameters
+    MAIL_PASSWORD = "UBU2JGC2wEqc"
 
 
 class DevConfig(Config):
     EMAIL_ADDRESS = 'vladprivalov1990@gmail.com'
-    REMOTE = "http://localhost:5000" # http://localhost:8080
+    REMOTE = "http://localhost:5000"
 
 
 class ProductionConfig(Config):
