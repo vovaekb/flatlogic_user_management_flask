ce4e7c367de0d86d2bc3939d75766e8ddeca1ad1
Vladimir Privalov <vovaprivalov@gmail.com>
2023-10-02 21:46:19 +0400

    Refactor code

--- test_auth.py
+++ test_auth.py
@@ -1,4 +1,6 @@
 import os
+import os
+from dotenv import load_dotenv
 from flask import json, jsonify
 from app import app
 
@@ -10,19 +12,22 @@ def test_signup():
     """
     print('testing /auth/signup')
     with app.test_client() as c:
-        rv = c.post('/auth/signup', json={
-           'email': 'ralf_stone@host.com',  # 'bill_xavier@host.com',
-            'password': 'jkht6fd4le,*',  # 'nnjk4cb&%d3', # 'dfgvd564rf'
-        })
+        rv = c.post(
+            '/auth/signup', 
+            json={
+                'email': 'ralf_stone@host.com',
+                'password': 'jkht6fd4le,*',
+            }
+        )
         print(rv.data)
         token = rv.data
         os.environ['AUTH_TOKEN'] = token.decode('utf-8')
-        print('status: ', rv.status_code)
+        print('status: %s' % rv.status_code)
 
     # Create new user from admin panel
     '''
     with app.test_client() as c:
-        token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTczOTA5NjEsImlhdCI6MTYxNzM2OTM2MSwiaWQiOiJiNTM1MGQ1YS03ZmYyLTQwM2ItOTMzNC00Y2UzNjQ0M2YzYTYiLCJlbWFpbCI6ImJpbGxfeGF2aWVyQGhvc3QuY29tIn0.GHzmEbzhMj5z3Zesr4wR_sIr9Od0SZ6SxRzO9gb780o'
+        token = os.environ['AUTH_TOKEN']
         authorization = 'Bearer ' + str(token)
 
         headers = {
@@ -30,14 +35,17 @@ def test_signup():
             # 'Content-Type': 'application/json',
             'Authorization': authorization
         }
-        password = 'sdvw3HGY' # '&hgvdsdf4xf'
-        rv = c.post('/auth/signup', json={
-            'email': 'uman_lesset@host.com', 'password': password
-        }, headers=headers)
-        # json_data = rv.get_json()
-        # print(json_data)
+        password = 'sdvw3HGY'
+        rv = c.post(
+            '/auth/signup', 
+            json={
+                'email': 'uman_lesset@host.com', 
+                'password': password
+            }, 
+            headers=headers
+        )
         print(rv.data)
-        print('status: ', rv.status_code)
+        print('status: %s' % rv.status_code)
     '''
 
 
@@ -45,16 +53,18 @@ def test_verify_email():
     """Test email verification endpoint
     """
     print('testing /verify-email')
-    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTc5Njk3NzUsImlhdCI6MTYxNzk2OTQxNSwic3ViIjoiN2YxZTVhMTAtNTdkNC00MGJmLThlZDAtODAzYzgzMTBlZjc3In0.oIFpXNmD2Ao1CT9GC9I426NUkmAHilVbV0RbUtFjtoQ'
-    # os.environ['EMAIL_VERIFY_TOKEN'] # AUTH_TOKEN']
+    token = os.environ['EMAIL_VERIFY_TOKEN']
     print(token)
     with app.test_client() as c:
-        rv = c.put('/auth/verify-email', json={
-            'token': token
-        })
+        rv = c.put(
+            '/auth/verify-email', 
+            json={
+                'token': token
+            }
+        )
         # json_data = rv.get_json()
         print(rv.data)
-        print('status: ' , rv.status_code)
+        print('status: %s' % rv.status_code)
         #print(json_data)
 
 
@@ -62,22 +72,21 @@ def test_send_email_address_verification_email():
     """Test sending email verification endpoint
     """
     print('test_send_email_address_verification_email')
-    #print(os.environ['AUTH_TOKEN'])
     with app.test_client() as c:
-        token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTc5OTA2MjAsImlhdCI6MTYxNzk2OTAyMCwiaWQiOiI3ZjFlNWExMC01N2Q0LTQwYmYtOGVkMC04MDNjODMxMGVmNzciLCJlbWFpbCI6InJhbGZfc3RvbmVAaG9zdC5jb20ifQ.Rzz27cK0SXsSA2Tgbg3V2lA5pnvL5BUzMHU0T4DL4s4' # os.environ['AUTH_TOKEN']
-        #token = os.environ['AUTH_TOKEN']
+        token = os.environ['AUTH_TOKEN']
         authorization = 'Bearer ' + str(token)
 
         headers = {
             # 'Content-Type': 'application/json',
             'Authorization': authorization
         }
-        password = 'sdvw3HGY'  # '&hgvdsdf4xf'
-        rv = c.post('/auth/send-email-address-verification-email', json={}, headers=headers)
-        # json_data = rv.get_json()
-        # print(json_data)
+        rv = c.post(
+            '/auth/send-email-address-verification-email', 
+            json={}, 
+            headers=headers
+        )
         print(rv.data)
-        print('status: ', rv.status_code)
+        print('status: %s' % rv.status_code)
 
 
 def test_signin_local():
@@ -85,27 +94,25 @@ def test_signin_local():
     """
     print('testing /signin/local')
     with app.test_client() as c:
-        rv = c.post('/auth/signin/local', json={
-            'email': 'ralf_stone@host.com', # 'billy_xavier@host.com', # 'billy_xavier@host.com',
-            'password': 'dfgvd564rf', # 'jkht6fd4le,*', # 'dfgvd564rf', #  'nnjk4cb&%d3', # 'dfgvd564rf'
-        })
-        json_data = rv.get_json()
-        #print(json_data)
+        rv = c.post(
+            '/auth/signin/local', json={
+                'email': 'ralf_stone@host.com',
+                'password': 'dfgvd564rf',
+            }
+        )
         token = rv.data
         os.environ['AUTH_TOKEN'] = token.decode('utf-8')
         token_file = 'auth_token.txt'
         with open(token_file, 'w') as f:
             f.write(token.decode('utf-8'))
         print(token)
-        print('status: ' , rv.status_code)
+        print('status: %s' % rv.status_code)
 
 def test_password_update():
     """Test updating password endpoint
     """
     print('testing /signin/password-update')
-    #print(os.environ['AUTH_TOKEN'])
-    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTc5OTA2MjAsImlhdCI6MTYxNzk2OTAyMCwiaWQiOiI3ZjFlNWExMC01N2Q0LTQwYmYtOGVkMC04MDNjODMxMGVmNzciLCJlbWFpbCI6InJhbGZfc3RvbmVAaG9zdC5jb20ifQ.Rzz27cK0SXsSA2Tgbg3V2lA5pnvL5BUzMHU0T4DL4s4' # os.environ['AUTH_TOKEN']
-    #token = os.environ['AUTH_TOKEN'] #token # 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTczODUxNjEsImlhdCI6MTYxNzM2MzU2MSwiaWQiOiIyYTE2ZTRmYy0xNmNkLTRlYTktOTNhZS0wZTIwZjg0ZWUzMjAiLCJlbWFpbCI6ImJpbGxfeGF2aWVyQGhvc3QuY29tIn0.49G21qLF1QFeE3y77z8FTwId5R7suxuDaitovl4oMoo'
+    token = os.environ['AUTH_TOKEN']
     authorization = 'Bearer ' + str(token)
 
     headers = {
@@ -114,36 +121,39 @@ def test_password_update():
         'Authorization': authorization
     }
     with app.test_client() as c:
-        rv = c.put('/auth/password-update', json={
-            'current_password': 'jkht6fd4le,*', # 'dfgvd564rf', # 'dfgvd564rf',
-            'new_password': 'dfgvd564rf', # '2as25Ifzr'
-        }, headers=headers)
-        json_data = rv.get_json()
+        rv = c.put(
+            '/auth/password-update', 
+            json={
+                'current_password': 'jkht6fd4le,*',
+                'new_password': 'dfgvd564rf', 
+            }, 
+            headers=headers
+        )
         print(rv.data)
-        print('status: ' , rv.status_code)
-        # print(json_data)
+        print('status: %s' % rv.status_code)
 
 def test_send_pasword_reset_email():
     """Test sending password reset email endpoint
     """
     with app.test_client() as c:
-        rv = c.post('/auth/send-password-reset-email', json={
-            'email': 'ralf_stone@host.com' # 'bill_xavier@host.com'
-        })
+        rv = c.post(
+            '/auth/send-password-reset-email', 
+            json={
+                'email': 'ralf_stone@host.com'
+            }
+        )
         print(rv.data)
-        print('status: ', rv.status_code)
+        print('status: %s' % rv.status_code)
 
 
 def test_password_reset():
     """Test password reset endpoint endpoint
     """
     print('testing /auth/password-reset')
-    #print(os.environ['AUTH_TOKEN'])
-    auth_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTc5OTA2MjAsImlhdCI6MTYxNzk2OTAyMCwiaWQiOiI3ZjFlNWExMC01N2Q0LTQwYmYtOGVkMC04MDNjODMxMGVmNzciLCJlbWFpbCI6InJhbGZfc3RvbmVAaG9zdC5jb20ifQ.Rzz27cK0SXsSA2Tgbg3V2lA5pnvL5BUzMHU0T4DL4s4' # os.environ['AUTH_TOKEN']
-    #auth_token = os.environ['AUTH_TOKEN'] # token # 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTc2MzcwNzQsImlhdCI6MTYxNzYxNTQ3NCwiaWQiOiIyMjhmNGJiZS0yM2VjLTRhZmYtYTg3NC0yNTZlMWM4ZGVjZmMiLCJlbWFpbCI6ImJpbGxfeGF2aWVyQGhvc3QuY29tIn0.B1FXZ-UfjKq-HILHZuoVTV6C3uzlYhq54HI9b5hKb4w'
+    auth_token = os.environ['AUTH_TOKEN']
     authorization = 'Bearer ' + str(auth_token)
 
-    password_reset_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTc5NzE2MDYsImlhdCI6MTYxNzk3MTI0Niwic3ViIjoiN2YxZTVhMTAtNTdkNC00MGJmLThlZDAtODAzYzgzMTBlZjc3In0.PWsReE41SFZB4cye_DFY-fXAJCZJ1VAur-aCFC3QUJQ'
+    password_reset_token = os.environ['PASSWORD_RESET_TOKEN']
 
     headers = {
         # 'Access-Control-Allow-Origin': '*',
@@ -152,48 +162,54 @@ def test_password_reset():
     }
     with app.test_client() as c:
         rv = c.put('/auth/password-reset', json={
-            'password': 'jkht6fd4le,*', # 'dfgvd564rf',
-            'token': password_reset_token, # '2as25Ifzr'
+            'password': 'jkht6fd4le,*',
+            'token': password_reset_token,
         }, headers=headers)
         print(rv.data)
-        print('status: ', rv.status_code)
+        print('status: %s' % rv.status_code)
 
 def test_profile():
     """Test user profile endpoint
     """
     print('testing /auth/profile')
-    print(os.environ['AUTH_TOKEN'])
     auth_token = os.environ['AUTH_TOKEN']
     authorization = 'Bearer ' + str(auth_token)
     id = '188b7d6a-3bbd-44bd-8e07-6be6bc4b1e1f'
 
+    image_paths = [
+        '79c2b036-2efb-4f82-bd8b-6158fe0f36de.jpeg',
+        '27e264dd-aa46-4c98-8c04-aecace218d9e.png'
+    ]
+
     profile_data = {
-        #'id': id,
-        'email': 'ralf_stone@host.com', #'bill_xavier@host.com',
-        'firstName': 'Tailor', # 'Billy',
-        'lastName': None, # 'Xavier1',
-        'phoneNumber': '250051342', # '2211945',
+        'email': 'ralf_stone@host.com',
+        'firstName': 'Tailor',
+        'lastName': None,
+        'phoneNumber': '250051342',
         'role': 'admin',
         'disabled': False,
         'avatar': [
             {
                 'id': '79c2b036-2efb-4f82-bd8b-6158fe0f36de',
-                'name': 'tynvnmm.jpeg', # 'test1.png',
+                'name': 'tynvnmm.jpeg',
                 'new': True,
                 'sizeInBytes': 321800,
-                'privateUrl': 'users/avatar/79c2b036-2efb-4f82-bd8b-6158fe0f36de.jpeg', # 'users/avatar/eb4b4851-a5e4-483a-a1d0-3f3feedae3a6.png',
-                'publicUrl': 'http://127.0.0.1:5000/files/download?privateUrl=users/avatar/avatar/79c2b036-2efb-4f82-bd8b-6158fe0f36de.jpeg' # 'http://127.0.0.1:5000/files/download?privateUrl=users/avatar/avatar/eb4b4851-a5e4-483a-a1d0-3f3feedae3a6.png'
+                'privateUrl': 'users/avatar/%s' % (image_paths[0]),
+                'publicUrl': 
+                '%s/files/download?privateUrl=users/avatar/avatar/%s' %
+                (app.config['REMOTE'], image_paths[0])
             },
             {
                 'id': '27e264dd-aa46-4c98-8c04-aecace218d9e',
                 'name': 'heyhere.png',
                 'new': True,
                 'sizeInBytes': 213500,
-                'privateUrl': 'users/avatar/27e264dd-aa46-4c98-8c04-aecace218d9e.png',
-                'publicUrl': 'http://127.0.0.1:5000/files/download?privateUrl=users/avatar/avatar/27e264dd-aa46-4c98-8c04-aecace218d9e.png'
+                'privateUrl': 'users/avatar/%s' % (image_paths[1]),
+                'publicUrl':
+                '%s/files/download?privateUrl=users/avatar/avatar/%s' %
+                (app.config['REMOTE'], image_paths[1])
             },
         ]
-        # 'avatar': []
     }
 
     headers = {
@@ -202,29 +218,35 @@ def test_profile():
         'Authorization': authorization
     }
     with app.test_client() as c:
-        rv = c.put('/auth/profile', json={
-            'profile': profile_data
-        }, headers=headers)
+        rv = c.put(
+            '/auth/profile', 
+            json={
+                'profile': profile_data
+            },
+            headers=headers
+        )
         print(rv.data)
-        print('status: ', rv.status_code)
+        print('status: %s' % rv.status_code)
 
 
 def test_me():
     '''Test updating password endpoint
     '''
     print('testing /auth/me')
-    #print(os.environ['AUTH_TOKEN'])
-    auth_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTc5OTA2MjAsImlhdCI6MTYxNzk2OTAyMCwiaWQiOiI3ZjFlNWExMC01N2Q0LTQwYmYtOGVkMC04MDNjODMxMGVmNzciLCJlbWFpbCI6InJhbGZfc3RvbmVAaG9zdC5jb20ifQ.Rzz27cK0SXsSA2Tgbg3V2lA5pnvL5BUzMHU0T4DL4s4' # os.environ['AUTH_TOKEN']
-    #auth_token = os.environ['AUTH_TOKEN']
+    auth_token = os.environ['AUTH_TOKEN']
     authorization = 'Bearer ' + str(auth_token)
 
     headers = {
         'Authorization': authorization
     }
     with app.test_client() as c:
-        rv = c.get('/auth/me', json={}, headers=headers)
+        rv = c.get(
+            '/auth/me', 
+            json={}, 
+            headers=headers
+        )
         print(rv.data)
-        print('status: ', rv.status_code)
+        print('status: %s' % rv.status_code)
 
 
 def test_email_configured():
@@ -232,14 +254,16 @@ def test_email_configured():
     """
     print('testing /auth/email-configured')
     with app.test_client() as c:
-        rv = c.get('/auth/email-configured', json={})
+        rv = c.get(
+            '/auth/email-configured', 
+            json={}
+        )
         print(rv.data)
-        print('status: ', rv.status_code)
+        print('status: %s' % rv.status_code)
 
 
 if __name__ == '__main__':
-    pass
-    #test_signup()
+    test_signup()
     #test_signin_local()
     #test_send_email_address_verification_email()
     #test_verify_email()
